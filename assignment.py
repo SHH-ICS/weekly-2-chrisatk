08f4756cf057a610a41945cbbe7e12cd7e5d442e
Chris Atkinson <46910324+chrisatk@users.noreply.github.com>
2024-10-18 14:48:54 +0000

    Assignment 2 Python Solution

--- assignment.py
+++ assignment.py
@@ -1,1 +1,18 @@
+import math
+prompt = "What is the radius of the circle? "
+redo = True
+while ( redo ):
+  radius = input( prompt )
+  try:
+    radius = float(radius)
+    if ( radius >= 0 ):
+      redo = False
+    else:
+      print('The radius must be a > or = 0 ')
+  except:
+    print('The radius must be a number')
 
+area = math.pi * radius ** 2
+circumference = 2 * math.pi * radius
+print( "The area of the circle is", area )
+print( "The circumference of the circle is", circumference )
